22ea91023037f7f7f4256b2dbc79d8fb6830e5fc
Catalin Marinas <catalin.marinas@gmail.com>
2005-09-07 10:45:13 +0100

    Add a copyright command to display the copyright terms
    Signed-off-by: Catalin Marinas <catalin.marinas@gmail.com>

--- stgit/main.py
+++ stgit/main.py
@@ -92,6 +92,7 @@ def print_help():
     print 'commands:'
     print '  help        print this message'
     print '  version     display version information'
+    print '  copyright   display copyright information'
     print
 
     cmds = commands.keys()
@@ -121,6 +122,9 @@ def main():
     if cmd in ['-v', '--version', 'version']:
         print 'Stacked GIT %s' % version
         print 'Python version %s' % sys.version
+        sys.exit(0)
+    if cmd in ['copyright']:
+        print __copyright__
         sys.exit(0)
     if not cmd in commands:
         print >> sys.stderr, 'Unknown command: %s' % cmd
