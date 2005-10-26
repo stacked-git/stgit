e030aa4aea34ae10c7fb69446013804ad12cf36b
Chuck Lever <cel@netapp.com>
2005-10-26 14:51:34 -0400

    Remove unused import in stgit/git.py
    The "glob" module isn't used in stgit/git.py, so don't import it.
    
    Signed-off-by: Chuck Lever <cel@netapp.com>

--- stgit/git.py
+++ stgit/git.py
@@ -18,7 +18,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 """
 
-import sys, os, glob, popen2
+import sys, os, popen2
 
 from stgit.utils import *
 
