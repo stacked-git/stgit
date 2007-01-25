00d468f51b4222d5b027c4140713b5c4c1ea1a2a
Catalin Marinas <catalin.marinas@gmail.com>
2007-01-25 22:28:38 +0000

    Synchronise the current patch if none specified for 'sync'
    Signed-off-by: Catalin Marinas <catalin.marinas@gmail.com>

--- stgit/commands/sync.py
+++ stgit/commands/sync.py
@@ -119,8 +119,10 @@ def func(parser, options, args):
         patches = applied
     elif len(args) != 0:
         patches = parse_patches(args, applied, ordered = True)
+    elif applied:
+        patches = [crt_series.get_current()]
     else:
-        parser.error('no patches specified')
+        parser.error('no patches applied')
 
     if not patches:
         raise CmdException, 'No patches to synchronise'
