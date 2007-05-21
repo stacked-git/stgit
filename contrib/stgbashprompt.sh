34a1b4c2988875ba064cd3a261f3d3b8c63d7f8e
Robin Rosenberg <robin.rosenberg@dewire.com>
2007-05-21 21:58:39 +0100

    Don't use / as separatar since it is common i branch names
    Don't use / as separatar since it is common i branch names
    
        Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- contrib/stgbashprompt.sh
+++ contrib/stgbashprompt.sh
@@ -9,8 +9,8 @@ if [ "$PS1" ]; then
 		ref=$(git-symbolic-ref HEAD 2> /dev/null) || return
 		br=${ref#refs/heads/}
 		top=$(tail -n 1 $git_dir/patches/$br/applied 2>/dev/null) \
-			&& top="/$top";
-		echo "[$br$top]"
+		top=${top:-(none)}
+		echo "[$top@$br]"
 	}
 	PS1='\u@\h:$(__prompt_git)\W\$ '
 fi
