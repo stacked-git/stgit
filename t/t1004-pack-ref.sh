91fb87a65f2eb14968fee9160804c0ad25c286c8
Karl Hasselström <kha@treskal.com>
2007-08-20 22:36:00 +0100

    New test: make sure that StGIT can 	handle packed refs
    It currently can't, which is why this test is needed. The situation
    has recently taken a turn for the worse, since git-gc nowadays packs
    refs by default.

---
+++ t/t1004-pack-ref.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Copyright (c) 2007 Karl Hasselström
+#
+
+test_description='Test that StGIT can handle packed refs'
+
+. ./test-lib.sh
+stg init
+
+test_expect_success \
+    'Pack refs and make sure that we can still see them' '
+    stg branch -c foo &&
+    [ $(stg branch -l | tee /dev/stderr | wc -l) -eq 2 ] &&
+    git pack-refs --all &&
+    [ $(stg branch -l | tee /dev/stderr | wc -l) -eq 2 ]
+'
+
+test_expect_success \
+    'Try to delete a branch whose ref has been packed' '
+    stg branch -d master
+'
+
+test_done
