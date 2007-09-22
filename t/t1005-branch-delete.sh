2bdde74e540085e2c115d6d3dd77afa6d54e78aa
Karl Hasselström <kha@treskal.com>
2007-09-22 10:23:37 +0200

    Add simple test for "stg branch --delete"
    Signed-off-by: Karl Hasselström <kha@treskal.com>

---
+++ t/t1005-branch-delete.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+test_description='Attempt to delete branches'
+
+. ./test-lib.sh
+
+stg init
+
+test_expect_success 'Create a branch (and switch to it)' '
+    stg branch --create foo
+    '
+
+test_expect_success 'Delete a branch' '
+    stg branch --delete master
+    '
+
+test_expect_success 'Make sure the branch ref was deleted' '
+    [ -z "$(git show-ref | grep master | tee /dev/stderr)" ]
+    '
+
+test_expect_success 'Make sure the branch config was deleted' '
+    [ -z "$(git config -l | grep branch\\.master | tee /dev/stderr)" ]
+    '
+
+test_expect_success 'Make sure the branch files were deleted' '
+    [ -z "$(find .git -type f | grep master | tee /dev/stderr)" ]
+    '
+
+test_done
