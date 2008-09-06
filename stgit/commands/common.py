a1923ca89d84e6e2d39c5b6de1411623c5551484
Karl Hasselström <kha@treskal.com>
2008-09-06 10:16:22 +0200

    Remove unneeded import
    Signed-off-by: Karl Hasselström <kha@treskal.com>

--- stgit/commands/common.py
+++ stgit/commands/common.py
@@ -19,8 +19,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 """
 
 import sys, os, os.path, re
-from optparse import OptionParser, make_option
-
 from stgit.exception import *
 from stgit.utils import *
 from stgit.out import *
