05c9b6c6d638721af4aa9c7a83e257902eaaa06f
Peter Grayson <jpgrayson@gmail.com>
2017-09-10 23:13:35 -0400

    Add compat module with text type
    The `text` type ends up being `str` for Python 3 and `unicode` for Python
    2. It is useful to have this type available for isinstance() tests as well
    as for various type conversions.
    
    Signed-off-by: Peter Grayson <jpgrayson@gmail.com>

---
+++ stgit/compat.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# With unicode_literals enabled, the type of a string literal will be `unicode`
+# for Python 2 and `str` for Python 3.
+text = type('')
