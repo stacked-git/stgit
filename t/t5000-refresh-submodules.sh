fc49d2ef7856bb7f431bca5fa64ee1dec3eaa4e8
Jacob Keller <jacob.e.keller@intel.com>
2017-11-14 04:36:33 -0800

    refresh: do not include submodules by default
    When refreshing patches, the default action is to include every path
    which is changed relative to the current working directory. This common
    workflow creates problems when one uses a submodule. Due to the nature
    of submodules, under some workflows they can often be out-of-sync, such
    as when working on new updates within a submodule.
    
    Accidental refreshes which include the submodule changes often occur,
    which leads to commits which unintentionally include changes to
    submodule pointers. These commits likely cause submodules to point to
    incorrect locations.
    
    Since it is not easy to convince many developers to always use "refresh
    --index", instead, modify the default refresh behavior.
    
    Instead of including all files, now check to make sure that we exclude
    submodule files.
    
    We can do this by using git ls-tree to list the files and their modes.
    To avoid re-running ls-tree many times, we'll use a compiled regex to
    match lines that look like submodules, and extract their filename.
    
    Add a new option to refresh "--submodules" which tells refresh to
    include submodules regardless. Additionally, add a "--no-submodules" and
    a stg.refreshsubmodules configuration so that users may opt to configure
    this behavior.
    
    Since the number of users who use submodules is likely small, make this
    the new default behavior.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>

---
+++ t/t5000-refresh-submodules.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 Intel Corporation
+#
+
+test_description='Refresh with submodules'
+
+. ./test-lib.sh
+
+test_expect_success 'refresh with a submodule does not include by default' '
+  test_create_repo foo &&
+  git submodule add ./foo foo &&
+  git commit -m "submodule" &&
+  stg init &&
+  (
+    cd foo &&
+    touch file1 &&
+    git add file1 &&
+    git commit -m "change in submodule"
+  ) &&
+  stg new p1 -m p1 &&
+  stg refresh &&
+  [ "$(stg status)" == " M foo" ]
+'
+
+test_expect_success 'refresh includes non-submodule changes' '
+  touch file2 &&
+  git add file2 &&
+  stg refresh &&
+  [ "$(stg status)" == " M foo" ]
+'
+
+test_expect_success 'refresh with --submodules' '
+  stg refresh --submodules &&
+  [ "$(stg status)" == "" ]
+'
+
+test_expect_success 'refresh --no-submodules overrides config' '
+  stg undo && stg undo &&
+  git config stgit.refreshsubmodules yes &&
+  stg refresh --no-submodules &&
+  [ "$(stg status)" == " M foo" ]
+'
+
+test_expect_success 'refresh with config' '
+   stg refresh &&
+   [ "$(stg status)" == "" ]
+'
+
+test_done
