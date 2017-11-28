a29fc8873fca30cb5b13d94743a9010de28e2610
Catalin Marinas <catalin.marinas@gmail.com>
2017-11-28 17:14:01 +0000

    License contrib/stgit.el under GPLv2 or later
    This is to make it compatible with other Emacs packages. The StGit
    itself is licensed under GPLv2 only.
    
    Reported-by: Jonas Bernoulli <jonas@bernoul.li>
    Acked-by: David Kågedal <david@kagedal.org>
    Acked-by: Karl Wiberg <kha@treskal.com>
    Acked-by: Gustav Hållberg <gustav@gmail.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@gmail.com>

--- contrib/stgit.el
+++ contrib/stgit.el
@@ -5,6 +5,19 @@
 ;; Author: David Kågedal <davidk@lysator.liu.se>
 ;; Homepage: http://www.procode.org/stgit
 ;; Version: 0.17.1
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
