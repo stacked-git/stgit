63781f2036dea1de0142ce44ce9496a797657570
Peter Grayson <pete@jpgrayson.net>
2018-04-18 23:35:10 -0400

    Call setup() directly in setup.py
    The __run_setup() wrapper is unnecessary and calling setup() directly is
    the conventional approach.
    
    Signed-off-by: Peter Grayson <pete@jpgrayson.net>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from distutils.core import setup
-import glob
+from glob import glob
 import os
 import sys
 
@@ -23,12 +23,14 @@ def __version_to_list(version):
         ver_list.append(n)
     return ver_list
 
+
 def __check_min_version(min_ver, ver):
     """Check whether ver is greater or equal to min_ver
     """
     min_ver_list = __version_to_list(min_ver)
     ver_list = __version_to_list(ver)
     return min_ver_list <= ver_list
+
 
 def __check_python_version():
     """Check the minimum Python version
@@ -39,6 +41,7 @@ def __check_python_version():
               % (version.python_min_ver, pyver), file=sys.stderr)
         sys.exit(1)
 
+
 def __check_git_version():
     """Check the minimum GIT version
     """
@@ -48,46 +51,6 @@ def __check_git_version():
         print('GIT version %s or newer required. Found %s'
               % (version.git_min_ver, gitver), file=sys.stderr)
         sys.exit(1)
-
-def __run_setup():
-    setup(name = 'stgit',
-          version = version.version,
-          license = 'GPLv2',
-          author = 'Catalin Marinas',
-          author_email = 'catalin.marinas@gmail.com',
-          url = 'http://www.procode.org/stgit/',
-          download_url = 'http://repo.or.cz/stgit.git',
-          description = 'Stacked GIT',
-          long_description = 'Push/pop utility on top of GIT',
-          scripts = ['stg'],
-          packages = list(map(str, ['stgit', 'stgit.commands', 'stgit.lib'])),
-          data_files = [
-            ('share/stgit/templates', glob.glob('templates/*.tmpl')),
-            ('share/stgit/examples', glob.glob('examples/*.tmpl')),
-            ('share/stgit/examples', ['examples/gitconfig']),
-            ('share/stgit/contrib', ['contrib/stgbashprompt.sh']),
-            ('share/stgit/completion', ['stgit-completion.bash'])
-          ],
-          classifiers = [
-              'Development Status :: 5 - Production/Stable',
-              'Environment :: Console',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'
-              'Natural Language :: English',
-              'Operating System :: OS Independent',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.6',
-              'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.3',
-              'Programming Language :: Python :: 3.4',
-              'Programming Language :: Python :: 3.5',
-              'Programming Language :: Python :: 3.6',
-              'Programming Language :: Python :: Implementation :: CPython',
-              'Programming Language :: Python :: Implementation :: PyPy',
-              'Topic :: Software Development :: Version Control',
-          ])
 
 
 # Check the minimum versions required
@@ -101,13 +64,52 @@ version.write_builtin_version()
 
 # generate the python command list
 with open('stgit/commands/cmdlist.py', 'w') as f:
-    commands.py_commands(commands.get_commands(allow_cached = False), f)
+    commands.py_commands(commands.get_commands(allow_cached=False), f)
 
 # generate the bash completion script
 with open('stgit-completion.bash', 'w') as f:
     completion.write_completion(f)
 
-__run_setup()
+setup(
+    name='stgit',
+    version=version.version,
+    license='GPLv2',
+    author='Catalin Marinas',
+    author_email='catalin.marinas@gmail.com',
+    url='http://www.procode.org/stgit/',
+    download_url='http://repo.or.cz/stgit.git',
+    description='Stacked GIT',
+    long_description='Push/pop utility on top of GIT',
+    scripts=['stg'],
+    packages=list(map(str, ['stgit', 'stgit.commands', 'stgit.lib'])),
+    data_files=[
+        ('share/stgit/templates', glob('templates/*.tmpl')),
+        ('share/stgit/examples', glob('examples/*.tmpl')),
+        ('share/stgit/examples', ['examples/gitconfig']),
+        ('share/stgit/contrib', ['contrib/stgbashprompt.sh']),
+        ('share/stgit/completion', ['stgit-completion.bash']),
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Version Control',
+    ],
+)
 
 # restore the old mask
 os.umask(old_mask)
