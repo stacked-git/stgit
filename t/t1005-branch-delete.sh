43b60000ed82146ca1979f4826f6faf9d7c87ae5
Peter Grayson <pete@jpgrayson.net>
2019-12-13 17:49:28 -0500

    Improve branch delete tests
    - Add tests to force delete a branch with patches.
    - Do post-delete checks immediately after delete test.
    - Wrap stack init in a test
    
    Signed-off-by: Peter Grayson <pete@jpgrayson.net>

--- t/t1005-branch-delete.sh
+++ t/t1005-branch-delete.sh
@@ -4,33 +4,25 @@ test_description='Attempt to delete branches'
 
 . ./test-lib.sh
 
-stg init
+test_expect_success 'Initialize repo' '
+    stg init &&
+    test_commit p0 &&
+    test_commit p1 &&
+    stg uncommit -n 2
+'
 
 test_expect_success 'Create a branch (and switch to it)' '
-    stg branch --create foo
-    '
-
-test_expect_success 'Delete a branch' '
-    stg branch --delete master
-    '
-
-test_expect_success 'Attempt to delete current branch' '
-    command_error stg branch --delete $(stg branch)
+    stg branch --clone foo
 '
 
-test_expect_success 'Invalid num args to delete' '
-    command_error stg branch --delete &&
-    command_error stg branch --delete foo extra
+test_expect_success 'Attempt to delete branch with patches' '
+    command_error stg branch --delete master 2>&1 |
+    grep -e "series still contains patches"
 '
 
-test_expect_success 'Create a non-StGIT branch and delete it' '
-    git branch bar &&
-    stg branch --delete bar
-    '
-
-test_expect_success 'Delete a nonexistent branch' '
-   stg branch --delete bar
-   '
+test_expect_success 'Force delete branch with patches' '
+    stg branch --delete --force master
+'
 
 test_expect_success 'Make sure the branch ref was deleted' '
     [ -z "$(git show-ref | grep master | tee /dev/stderr)" ]
@@ -43,5 +35,27 @@ test_expect_success 'Make sure the branch config was deleted' '
 test_expect_success 'Make sure the branch files were deleted' '
     [ -z "$(find .git -type f | grep master | tee /dev/stderr)" ]
     '
+
+test_expect_success 'Attempt to delete current branch' '
+    command_error stg branch --delete $(stg branch) 2>&1 |
+    grep -e "Cannot delete the current branch"
+'
+
+test_expect_success 'Invalid num args to delete' '
+    command_error stg branch --delete 2>&1 |
+    grep -e "incorrect number of arguments" &&
+    command_error stg branch --delete foo extra 2>&1 |
+    grep -e "incorrect number of arguments"
+'
+
+test_expect_success 'Create a non-StGIT branch and delete it' '
+    git branch bar &&
+    stg branch --delete bar
+'
+
+test_expect_success 'Delete a nonexistent branch' '
+   command_error stg branch --delete bar 2>&1 |
+   grep -e "no such branch"
+'
 
 test_done
