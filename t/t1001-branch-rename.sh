5f4c7b08cdfc3f85e0aa51e5f66a5a510d46d7c5
Peter Grayson <pete@jpgrayson.net>
2019-12-13 17:36:30 -0500

    Improved branch rename tests
    More post-rename checking to ensure that various bits and pieces were
    renamed/moved correctly.
    
    Also add tests for renaming the current branch. These test currently fail,
    but will work when branching commands are updated to new lib infra.
    
    Signed-off-by: Peter Grayson <pete@jpgrayson.net>

--- t/t1001-branch-rename.sh
+++ t/t1001-branch-rename.sh
@@ -11,29 +11,49 @@ Exercises branch renaming commands.
 . ./test-lib.sh
 
 test_expect_success \
-    'Create an stgit branch from scratch' \
-    'stg init &&
-     stg branch -c foo &&
-     stg new p1 -m "p1"
+    'Create an stgit branch from scratch' '
+    stg init &&
+    stg branch -c foo &&
+    stg new p1 -m "p1"
 '
 
 test_expect_success \
-    'Rename the current stgit branch' \
-    'command_error stg branch -r foo bar
+    'Rename a stgit branch' '
+    git config --get-regexp branch\\.foo\\. &&
+    stg branch -c buz &&
+    stg branch -r foo bar &&
+    test_expect_code 1 git config --get-regexp branch\\.foo\\. &&
+    git config --get-regexp branch\\.bar\\. &&
+    test_path_is_file .git/refs/heads/bar &&
+    test_path_is_file .git/refs/heads/bar.stgit
+'
+
+test_expect_failure \
+    'Rename the current stgit branch' '
+    stg branch bar &&
+    git config --get-regexp branch\\.bar\\. &&
+    test_expect_code 1 git config --get-regexp branch\\.foo\\. &&
+    stg branch -r foo &&
+    git config --get-regexp branch\\.foo\\. &&
+    test_path_is_file .git/refs/heads/foo &&
+    test_path_is_file .git/refs/heads/foo.stgit &&
+    test_expect_code 1 git config --get-regexp branch\\.bar\\. &&
+    test_path_is_missing .git/refs/heads/bar &&
+    test_path_is_missing .git/refs/heads/bar.stgit &&
+    test "$(stg series --noprefix --applied)" = "p1" &&
+    test "$(git symbolic-ref --short HEAD)" = "foo"
+'
+
+test_expect_failure \
+    'Rename the current stgit branch single arg' '
+    stg branch -r xxx &&
+    test "$(stg branch)" = "xxx"
 '
 
 test_expect_success \
-    'Invalid num args to rename' \
-    'command_error stg branch --rename baz 2>&1 |
-     grep "incorrect number of arguments"
-'
-
-test_expect_success \
-    'Rename an stgit branch' \
-    'stg branch -c buz &&
-     stg branch -r foo bar &&
-     [ -z "$(find .git -type f | grep foo | tee /dev/stderr)" ] &&
-     test -z "$(git config -l | grep branch\\.foo)"
+    'Invalid num args to rename' '
+    command_error stg branch --rename bar biz bop 2>&1 |
+    grep "incorrect number of arguments"
 '
 
 test_done
