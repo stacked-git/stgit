f547b4098077b8843cba95c2ddc1c56fb69251f5
Peter Grayson <pete@jpgrayson.net>
2020-11-15 18:10:35 -0500

    Minor improvements to contrib scripts
    stg-show: remove unused endofpatches variable
    
    stg-mdiff: quote "$0"
    
    stg-unnew: Use `stg pop --spill` instead of pick
    
    stgbashprompt.sh: Use `stg branch` and `stg top` instead of probing
    stgit metadata directly.
    
    Signed-off-by: Peter Grayson <pete@jpgrayson.net>

--- contrib/stgbashprompt.sh
+++ contrib/stgbashprompt.sh
@@ -4,12 +4,9 @@
 if [ "$PS1" ]; then
 	function __prompt_git()
 	{
-		local git_dir ref br top;
-		git_dir=$(git rev-parse --git-dir 2> /dev/null) || return
-		ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-		br=${ref#refs/heads/}
-		top=$(tail -n 1 $git_dir/patches/$br/applied 2>/dev/null) \
-		top=${top:-(none)}
+		local br top
+		br=$(stg branch 2>/dev/null) || return
+		top=$(stg top 2>/dev/null) || return
 		echo "[$top@$br]"
 	}
 	PS1='\u@\h:$(__prompt_git)\W\$ '
