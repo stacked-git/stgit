stacked-git/stgit

commit 78f80f48ba33c9adfc0694d2065f001b638d0345

Fix mail.py cover letter generation - diffstat
The cover letter generator is broken: %(diffstat)s diffs the entire
stack instead of the range of patches requested on the command line.

Fixes: 5b99accc4e92 ("Migrate `stg mail` to new lib infrastructure")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

stgit/commands/mail.py
@@ -607,6 +607,17 @@ def __shortlog(stack, patches):
     ).raw_input(log).raw_output()
 
 
+def __diffstat(stack, patches, diff_opts):
+    rev1 = stack.patches.get(patches[0])
+    rev2 = stack.patches.get(patches[-1])
+    return stack.repository.diff_tree(
+            rev1.commit.data.tree,
+            rev2.commit.data.tree,
+            diff_opts,
+            stat=True,
+    )
+
+
 def __build_cover(tmpl, msg_id, options, patches):
     """Build the cover message (series description) to be sent via SMTP
     """
@@ -656,12 +667,7 @@ def __build_cover(tmpl, msg_id, options, patches):
         'nspace': number_space,
         'snumber': number_str.strip(),
         'shortlog': __shortlog(stack, patches),
-        'diffstat': repository.diff_tree(
-            stack.base.data.tree,
-            stack.top.data.tree,
-            diff_opts=options.diff_flags,
-            stat=True,
-        ),
+        'diffstat': __diffstat(stack, patches, options.diff_flags),
     }
 
     try:

commit 7af6574f3dd432653a48261bba722def783ab6ee

mail: use the whole series when generating the diffstat
when sending an email using stg mail including a cover letter,
a template may be used, and such template may include %(diffstat)s.
Supposing you are sending a single patch like:

stg mail --to "author@example.com" --c ~/.stgit/templates/covermail.tmpl -- foo-patch

the expectation is that such variable, if present, gets expanded
with the diff statistics, but actually no stats get included in
the cover letter.

The issue is also present sending multiple patches. In such case
the first patch in the series gets excluded from the statistics.

Fix it passing the first patch's parent reference to diff_tree().

Fixes: 78f80f48ba33 ("Fix mail.py cover letter generation - diffstat")
Signed-off-by: Paolo Valerio <paolo@contextswitch.info>

stgit/commands/mail.py
@@ -583,7 +583,7 @@ def __shortlog(stack, patches):
 
 
 def __diffstat(stack, patches):
-    tree1 = stack.patches[patches[0]].data.tree
+    tree1 = stack.patches[patches[0]].data.parent.data.tree
     tree2 = stack.patches[patches[-1]].data.tree
     return stack.repository.diff_tree(
         tree1,