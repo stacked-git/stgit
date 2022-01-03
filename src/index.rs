3421de7e1b290ce2f2334e617e5c6144d4e784f0
Peter Grayson <pete@jpgrayson.net>
2022-01-03 16:53:32 -0500

    Add file-based temp index to TemporaryIndex trait
    In order to use git commands to perform operations against a temporary
    index, that temporary index must exist in the filesystem. The new
    with_temp_index_file() function provides this capability.

--- src/index.rs
+++ src/index.rs
@@ -4,6 +4,10 @@ pub(crate) trait TemporaryIndex {
     fn with_temp_index<F, T>(&self, f: F) -> Result<T, Error>
     where
         F: FnOnce(&mut git2::Index) -> Result<T, Error>;
+
+    fn with_temp_index_file<F, T>(&self, f: F) -> Result<T, Error>
+    where
+        F: FnOnce(&mut git2::Index) -> Result<T, Error>;
 }
 
 impl TemporaryIndex for git2::Repository {
@@ -17,6 +21,23 @@ impl TemporaryIndex for git2::Repository {
         let result = f(&mut temp_index);
         self.set_index(&mut orig_index)
             .expect("can reset to original index");
+        result
+    }
+
+    fn with_temp_index_file<F, T>(&self, f: F) -> Result<T, Error>
+    where
+        F: FnOnce(&mut git2::Index) -> Result<T, Error>,
+    {
+        // TODO: dynamic file name
+        let temp_index_path = self.path().join("index-temp-stgit");
+        let mut temp_index = git2::Index::open(&temp_index_path)?;
+        let mut orig_index = self.index()?;
+        temp_index.write()?;
+        self.set_index(&mut temp_index)?;
+        let result = f(&mut temp_index);
+        self.set_index(&mut orig_index)
+            .expect("can reset to original index");
+        std::fs::remove_file(&temp_index_path).expect("can remove temp index file");
         result
     }
 }
