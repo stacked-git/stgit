4b5e435816ff40d1e03add9ac974b279a4030435
Peter Grayson <pete@jpgrayson.net>
2022-08-14 18:54:36 -0400

    Show git commit id with version when needed
    The version of StGit displayed by `stg version` is primarily informed by
    the package version from Cargo.toml. A problem, however, is that only for
    proper (tagged) StGit releases will the package version correspond with the
    git tag from which it was built. Thus any one-off build of StGit will lie
    about its version.
    
    To solve this, a build.rs script is added that looks at `git describe`
    output to determine whether the `stg version` version should be
    supplemented with the git commit id (hash) from whence it was built.
    
    When StGit is built from a tag and that tag matches the package version
    from Cargo.toml, the git hash is elided from `stg version`. Otherwise, `stg
    version` shows the hash as a parenthetical supplement to the
    Cargo.toml-supplied version. If the working tree is dirty, the hash is
    supplemented with an asterisk.
    
    When building from outside a git repo, e.g. from a release .tar.gz archive,
    the build script falls back to using the only version available to it: the
    package version from Cargo.toml.

---
+++ build.rs
@@ -0,0 +1,82 @@
+fn main() {
+    let git_output = std::process::Command::new("git")
+        .args(["rev-parse", "--git-dir"])
+        .output()
+        .ok();
+    let git_dir = git_output.as_ref().and_then(|output| {
+        std::str::from_utf8(&output.stdout)
+            .ok()
+            .and_then(|s| s.strip_suffix('\n').or_else(|| s.strip_suffix("\r\n")))
+    });
+
+    // Tell cargo to rebuild if the head or any relevant refs change.
+    if let Some(git_dir) = git_dir {
+        println!("cargo:rerun-if-changed={git_dir}/HEAD");
+        println!("cargo:rerun-if-changed={git_dir}/refs/heads");
+        println!("cargo:rerun-if-changed={git_dir}/refs/tags");
+        println!("cargo:rerun-if-changed={git_dir}/packed-refs");
+    }
+
+    let git_output = std::process::Command::new("git")
+        .args([
+            "describe",
+            "--match=v*",
+            "--abbrev=9",
+            "--long",
+            "--dirty=*",
+        ])
+        .output()
+        .ok();
+    let git_hash = git_output
+        .as_ref()
+        .and_then(|output| std::str::from_utf8(&output.stdout).ok().map(|s| s.trim()))
+        // E.g. "v2.0.0-beta.2-7-g12ab34cd56*"
+        //       ttttttttttttt N hhhhhhhhhhhD
+        //  t => last matching annotated tag
+        //  N => distance (in commits) from tag
+        //  h => git commit id (hash)
+        //  D => work tree dirty flag
+        //
+        // N.B. Since the tag may have '-' characters, the description string is parsed
+        // from right to left.
+        .and_then(|desc| {
+            desc.rsplit_once('-').and_then(|(tag_and_distance, hash)| {
+                let hash = hash
+                    .strip_prefix('g')
+                    .expect("git hash component starts with 'g'");
+                let dirty = hash.ends_with('*');
+                tag_and_distance
+                    .rsplit_once('-')
+                    .and_then(|(tag, distance_from_tag)| {
+                        let tag = tag
+                            .strip_prefix('v')
+                            .expect("last annotated tag starts with 'v'");
+                        let distance_from_tag = distance_from_tag
+                            .parse::<usize>()
+                            .expect("distance from tag is usize");
+                        let cargo_pkg_version = env!("CARGO_PKG_VERSION");
+
+                        if tag != cargo_pkg_version {
+                            println!(
+                                "cargo:warning=\
+                                 version from Cargo.toml, \"{cargo_pkg_version}\", \
+                                 does not match last annotated tag \"{tag}\""
+                            );
+                        }
+
+                        if tag == cargo_pkg_version && distance_from_tag == 0 && !dirty {
+                            // Do not use/display hash when the cargo version exactly
+                            // matches the latest annotated tag.
+                            None
+                        } else {
+                            Some(hash)
+                        }
+                    })
+            })
+        });
+
+    // Make the current git hash available to the build.
+    if let Some(git_hash) = git_hash {
+        println!("cargo:rustc-env=STGIT_BUILD_GIT_HASH={git_hash}");
+    }
+}
--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -30,17 +30,21 @@ fn make() -> clap::Command<'static> {
 }
 
 fn run(matches: &ArgMatches) -> Result<()> {
+    let pkg_version = env!("CARGO_PKG_VERSION");
+    let hash_suffix = option_env!("STGIT_BUILD_GIT_HASH")
+        .and_then(|rev_hash| Some(format!(" ({rev_hash})")))
+        .unwrap_or_default();
     if matches.contains_id("short") {
-        println!("{} {}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION"));
+        let bin_name = env!("CARGO_BIN_NAME");
+        println!("{bin_name} {pkg_version}{hash_suffix}");
     } else {
+        let license_id = env!("CARGO_PKG_LICENSE");
         println!(
-            "Stacked Git {}\n\
+            "Stacked Git {pkg_version}{hash_suffix}\n\
              Copyright (C) 2005-2022 StGit authors\n\
              This is free software: you are free to change and redistribute it.\n\
              There is NO WARRANTY, to the extent permitted by law.\n\
-             SPDX-License-Identifier: {}",
-            env!("CARGO_PKG_VERSION"),
-            env!("CARGO_PKG_LICENSE"),
+             SPDX-License-Identifier: {license_id}",
         );
         println!("{}", StupidContext::default().version()?);
     }
