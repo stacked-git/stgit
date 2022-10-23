fe2e485792930843eccc1481104d000f051c2254
Peter Grayson <pete@jpgrayson.net>
2022-10-23 11:10:15 -0400

    style: Normalize style in t2103-pull-trailing.sh

--- t/t2103-pull-trailing.sh
+++ t/t2103-pull-trailing.sh
@@ -3,9 +3,7 @@
 # Copyright (c) 2006 Yann Dirson
 #
 
-test_description='test
-
-'
+test_description='Test pull behaviors'
 
 . ./test-lib.sh
 
@@ -15,54 +13,56 @@ test_description='test
 # Need a repo to clone
 test_create_repo foo
 
-test_expect_success \
-    'Setup and clone tree, and setup changes' \
-    "(
-         cd foo &&
-         printf 'a\nb\n' > file &&
-         stg add file &&
-         git commit -m .
-     ) &&
-     git clone foo bar &&
-     (
-         cd bar &&
-         stg init &&
-         stg new p1 -m p1 &&
-         printf 'c\n' >> file &&
-         stg refresh
-     )
-"
+test_expect_success 'Setup and clone tree, and setup changes' '
+    (
+        cd foo &&
+        echo a >file &&
+        echo b >>file &&
+        stg add file &&
+        git commit -m .
+    ) &&
+    git clone foo bar &&
+    (
+        cd bar &&
+        stg init &&
+        stg new p1 -m p1 &&
+        echo c >> file &&
+        stg refresh
+    )
+'
 
-test_expect_success \
-    'Port those patches to orig tree' \
-    '(cd foo &&
-      GIT_DIR=../bar/.git git format-patch --stdout \
-          $(cd ../bar && stg id master:{base})..HEAD |
-      git am -3 -k
-     )
-    '
+test_expect_success 'Port those patches to orig tree' '
+    (
+        cd foo &&
+        git -C ../bar format-patch --stdout $(stg -C ../bar id master:{base})..HEAD |
+        git am -3 -k
+    )
+'
 
-test_expect_success \
-    'Pull those patches applied upstream, without pushing' \
-    "(cd bar && stg pull --nopush
-     )
-"
+test_expect_success 'Pull those patches applied upstream, without pushing' '
+    (
+        cd bar &&
+        stg pull --nopush
+    )
+'
 
-test_expect_success \
-    'Try to push those patches without merge detection' \
-    "(cd bar && stg push --all
-     )
-"
+test_expect_success 'Try to push those patches without merge detection' '
+    (
+        cd bar &&
+        stg push --all
+    )
+'
 
-test_expect_success \
-    'Pull those patches applied upstream' \
-    "(cd bar && stg undo && stg push --all --merged
-     )
-"
+test_expect_success 'Pull those patches applied upstream' '
+    (
+        cd bar &&
+        stg undo &&
+        stg push --all --merged
+    )
+'
 
-test_expect_success \
-    'Check that all went well' \
-    "test_cmp foo/file bar/file
-"
+test_expect_success 'Check that all went well' '
+    test_cmp foo/file bar/file
+'
 
 test_done
