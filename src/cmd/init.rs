3de648c2c62ac3ff4c030a42a0c9e67b83a1af9c
Peter Grayson <pete@jpgrayson.net>
2022-11-14 11:06:08 -0500

    docs(init): Add long help for `stg init`
    This expands on the long about that had not been ported from the Python
    implementation. Includes statement about deinitializing with `stg branch
    --cleanup`.
    
    Fixes: #233

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -15,7 +15,22 @@ pub(super) const STGIT_COMMAND: super::StGitCommand = super::StGitCommand {
 };
 
 fn make() -> clap::Command {
-    clap::Command::new(STGIT_COMMAND.name).about("Initialize a StGit stack on current branch")
+    clap::Command::new(STGIT_COMMAND.name)
+        .about("Initialize a StGit stack on current branch")
+        .long_about(
+            "Initialize a StGit stack on the current branch.\n\
+             \n\
+             A branch must be initialized with a StGit stack before patches may be \
+             created with 'stg new', imported with 'stg import', or picked with 'stg \
+             pick'.\n\
+             \n\
+             The branch and its git repository must already exist and contain at least \
+             one commit before initializing a StGit stack. Branches created with `stg \
+             branch --create` are automatically initialized.\n\
+             \n\
+             StGit stack metadata can be deinitialized from a branch using `stg branch \
+             --cleanup`. See 'stg branch' for more details.",
+        )
 }
 
 fn run(_: &ArgMatches) -> Result<()> {
