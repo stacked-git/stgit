stacked-git/stgit

commit 04559f055298df4485116640a3ea76b8e860a0f5

stgit.el: Enable lexical-binding

contrib/stgit.el
@@ -1,4 +1,4 @@
-;;; stgit.el --- major mode for StGit interaction
+;;; stgit.el --- major mode for StGit interaction  -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2007-2013 David Kågedal
 ;;
@@ -543,20 +543,18 @@ been advised to update the stgit status when necessary.")
     (ewoc-invalidate (car stgit-worktree-node) (cdr stgit-worktree-node))))
 
 (defun stgit-run-series-insert-index (ewoc)
-  ;; TODO: non-lexical stuff happening here (`index-node' and `worktree-node').
-  ;; Fix this before enabling lexical binding.
-  (setq index-node    (cons ewoc (ewoc-enter-last ewoc
-                                                  (make-stgit-patch
-                                                   :status 'index
-                                                   :name :index
-                                                   :desc nil
-                                                   :empty nil)))
-        worktree-node (cons ewoc (ewoc-enter-last ewoc
-                                                  (make-stgit-patch
-                                                   :status 'work
-                                                   :name :work
-                                                   :desc nil
-                                                   :empty nil)))))
+  (setq stgit-index-node    (cons ewoc (ewoc-enter-last ewoc
+                                                        (make-stgit-patch
+                                                         :status 'index
+                                                         :name :index
+                                                         :desc nil
+                                                         :empty nil)))
+        stgit-worktree-node (cons ewoc (ewoc-enter-last ewoc
+                                                        (make-stgit-patch
+                                                         :status 'work
+                                                         :name :work
+                                                         :desc nil
+                                                         :empty nil)))))
 
 (defun stgit-get-position (&optional position)
   "Return `stgit-mode' position information at POSITION (point by
@@ -708,9 +706,7 @@ using (make-hash-table :test \='equal)."
                                     :desc   desc
                                     :empty  empty)))
                 (forward-line 1)))))))
-    (let ((inserted-index (not stgit-show-worktree))
-          index-node
-          worktree-node)
+    (let ((inserted-index (not stgit-show-worktree)))
       (with-temp-buffer
         (let* ((standard-output (current-buffer))
                (exit-status (stgit-run-silent "series"
@@ -752,9 +748,7 @@ using (make-hash-table :test \='equal)."
               (forward-line 1)))))
       (unless inserted-index
         (stgit-run-series-insert-index ewoc))
-      (setq stgit-index-node     index-node
-            stgit-worktree-node  worktree-node
-            stgit-marked-patches (cl-intersection stgit-marked-patches
+      (setq stgit-marked-patches (cl-intersection stgit-marked-patches
                                                all-patchsyms)))))
 
 (defun stgit-current-branch ()

commit 866fe78d21c0284acd69c07282c6121ad2d2be09

stgit.el: Fix Index/Work Tree incremental refresh
Index/Work Tree incremental refresh has been broken in center mode after
lexical-binding change made in commit 04559f055298df4485116640a3ea76b8e860a0f5.

When stgit-show-worktree-mode is center, stgit-run-series-insert-index
may be called while parsing stg series output inside with-temp-buffer.
Since stgit-index-node and stgit-worktree-node are buffer-local, the
current implementation stores them in the temporary buffer, not the real
stgit-mode buffer.

As a result, after reload in this layout:

stgit-index-node    ;; nil in the stgit buffer
stgit-worktree-node ;; nil in the stgit buffer

Then commands like stgit-toggle-index call:

(stgit-refresh-worktree)
(stgit-refresh-index)

but both are no-ops because the node variables are nil. A full
stgit-reload still works, which is why pressing g fixes the display.

Fix by applying the updated stgit-index-node and stgit-worktree-node to
the real stgit-mode buffer instead.

Fixes: 04559f055298df4485116640a3ea76b8e860a0f5
Fixes: #633

Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

contrib/stgit.el
@@ -573,18 +573,21 @@ been advised to update the stgit status when necessary.")
     (ewoc-invalidate (car stgit-worktree-node) (cdr stgit-worktree-node))))
 
 (defun stgit-run-series-insert-index (ewoc)
-  (setq stgit-index-node    (cons ewoc (ewoc-enter-last ewoc
-                                                        (make-stgit-patch
-                                                         :status 'index
-                                                         :name :index
-                                                         :desc nil
-                                                         :empty nil)))
-        stgit-worktree-node (cons ewoc (ewoc-enter-last ewoc
-                                                        (make-stgit-patch
-                                                         :status 'work
-                                                         :name :work
-                                                         :desc nil
-                                                         :empty nil)))))
+  (let ((index-node (ewoc-enter-last ewoc
+                                     (make-stgit-patch
+                                      :status 'index
+                                      :name :index
+                                      :desc nil
+                                      :empty nil)))
+        (worktree-node (ewoc-enter-last ewoc
+                                        (make-stgit-patch
+                                         :status 'work
+                                         :name :work
+                                         :desc nil
+                                         :empty nil))))
+    (with-current-buffer (ewoc--buffer ewoc)
+      (setq stgit-index-node (cons ewoc index-node)
+            stgit-worktree-node (cons ewoc worktree-node)))))
 
 (defun stgit-get-position (&optional position)
   "Return position information at POSITION or point that can be restored later.